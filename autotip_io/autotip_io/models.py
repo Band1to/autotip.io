e221d6406726248af30c93eaabcbce5d80b55773
Chris Priest <cp368202@ohiou.edu>
2015-03-21 15:20:22 -0700

    work in progress

--- autotip_io/autotip_io/models.py
+++ autotip_io/autotip_io/models.py
@@ -1,10 +1,17 @@
 import datetime
+import random
 from django.db import models
 
 class GiveawaySubmission(models.Model):
     date_created = models.DateTimeField(default=datetime.datetime.now)
     address = models.TextField()
     winner = models.BooleanField(default=False)
+
+    def is_eligible(self):
+        """
+        Consult the blockchain to see if they are actually tipping.
+        """
+        return True
 
 def draw_winner(drawing_date):
     """
@@ -13,17 +20,13 @@ def draw_winner(drawing_date):
     """
     week_ago = drawing_date - datetime.timedelta(days=7)
 
-    random_submission = GiveawaySubmission.objects.filter(
+    all_submissions = GiveawaySubmission.objects.filter(
         date_created__gt=week_ago,
         date_created__lt=drawing_date,
         winner=False
-    ).order_by("?")[1]
+    )
 
-    eligible = []
-    for gs in all_submissions:
-        if gs.is_eligible():
-            eligible.append(gs)
-
-    random.choice()
-
-    return winner
+    while True:
+        candidate = random.choice(all_submissions)
+        if candidate.is_eligible():
+            return candidate
