0cec48d12f64de1e8e42f3c9a4ec59f1444b04c6
Chris Priest <cp368202@ohiou.edu>
2015-03-22 17:30:35 -0700

    changed urls

--- autotip_io/autotip_io/urls.py
+++ autotip_io/autotip_io/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'autotip_io.views.home', name='home'),
     url(r'^blog/', 'autotip_io.views.blog'),
-    url(r'^giveaway_submission', 'autotip_io.views.giveaway_submission'),
-    url(r'^giveaway_rules', 'autotip_io.views.giveaway_rules'),
+    url(r'^giveaway/submission', 'autotip_io.views.giveaway_submission'),
+    url(r'^giveaway/info', 'autotip_io.views.giveaway_rules'),
     url(r'^admin/', include(admin.site.urls)),
 )
