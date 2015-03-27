8e4e741a196e6928265ed30c4e3dbe4b74cc3a70
Chris Priest <cp368202@ohiou.edu>
2015-03-27 11:27:07 -0700

    added blockchain to admin

--- autotip_io/autotip_io/admin.py
+++ autotip_io/autotip_io/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Blog, GiveawaySubmission
 
 class GiveawaySubmissionAdmin(admin.ModelAdmin):
-    list_display = ('date_created', 'address')
+    list_display = ('date_created', 'address', 'blockchain')
+
+    def blockchain(self, obj):
+        url = "https://blockchan.info/address/%s" % obj.address
+        return "<a href='%s'>Link</a>" % url
+    blockchain.allow_tags = True
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('date_created', 'title', 'author')
