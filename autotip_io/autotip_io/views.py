24901ee3de779f7516d1d679503255f4f4cae1ab
Chris Priest <nbvfour@gmail.com>
2017-04-03 16:12:52 -0700

    reverse order

--- autotip_io/autotip_io/views.py
+++ autotip_io/autotip_io/views.py
@@ -43,5 +43,5 @@ def single_article(request, pk):
     return TemplateResponse(request, 'single_article_or_blog.html', {'article': article})
 
 def article_index(request):
-    articles = Article.objects.order_by('date_created')
+    articles = Article.objects.order_by('-date_created')
     return TemplateResponse(request, 'article_index.html', {'articles': articles})
